ba014ba73c3095ea7a1f411262b82b0a714a8c7f
Gagandeep Singh <gagandeep16037@iiitd.ac.in>
2019-09-08 22:38:01 +0530

    Complete predictions of spam or ham email. ToDo: predict from the model.py

---
+++ app.py
@@ -0,0 +1,79 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+def text_process(mess):
+    no_punc = [c for c in mess if c not in string.punctuation]
+    no_punc = ''.join(no_punc)
+    return [word for word in no_punc.split(' ') if word.lower() not in stopwords.words('english')]
+pipeline = Pipeline([
+    # strings to token integer counts
+    ('vectorMatrix', CountVectorizer(analyzer=text_process)),
+    ('tfidf', TfidfTransformer())
+])
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    messages = pd.read_csv('smsspamcollection/SMSSpamCollection', sep='\t',
+                           names=["label", "message"])
+    
+    vectorMatrix = CountVectorizer(analyzer=text_process)
+
+
+    messageVector = vectorMatrix.fit_transform(messages['message'])
+
+    #tfidf
+    tfidf = TfidfTransformer()
+    message_tfidf = tfidf.fit_transform(messageVector)
+
+    #train test split
+    # msg_train, msg_test, label_train, label_test = train_test_split(
+    #     message_tfidf, messages['label'], test_size=0.33)
+
+
+    # fitting on train data
+    spam_detect_model = MultinomialNB().fit(message_tfidf, messages['label'])
+
+    #app.py from here
+    #message from form
+    message = [request.form['message']]
+
+    # #creating vector and tdif
+    # vectorMatrix = CountVectorizer(analyzer=text_process)
+    # tfidf = TfidfTransformer()
+
+    #transforming data
+    vector = vectorMatrix.transform(message)
+    data = tfidf.transform(vector).toarray()
+
+    #predictiong through the model
+    predictions = model.predict(data)
+
+    return render_template('index.html', prediction_text='The text message is {}'.format(predictions))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- model.py
+++ model.py
@@ -8,6 +8,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+
 messages = pd.read_csv('smsspamcollection/SMSSpamCollection', sep='\t',
                        names=["label", "message"])
 print(len(messages))
@@ -20,12 +22,12 @@ def text_process(mess):
 # messages['message'].apply(text_process)
 
 #creating a matrix of count of each word in each message
-vectorMatrix=CountVectorizer(analyzer=text_process).fit(messages['message'])
-messageVector = vectorMatrix.transform(messages['message'])
+vectorMatrix=CountVectorizer(analyzer=text_process)
+messageVector = vectorMatrix.fit_transform(messages['message'])
 
 #tfidf
-tfidf = TfidfTransformer().fit(messageVector)
-message_tfidf = tfidf.transform(messageVector)
+tfidf = TfidfTransformer()
+message_tfidf = tfidf.fit_transform(messageVector)
 
 #train test split
 # msg_train, msg_test, label_train, label_test = train_test_split(
@@ -39,4 +41,18 @@ spam_detect_model = MultinomialNB().fit(message_tfidf, messages['label'])
 
 pickle.dump(spam_detect_model, open('model.pkl', 'wb'))
 
+pipeline = Pipeline([
+    # strings to token integer counts
+    ('vectorMatrix', CountVectorizer(analyzer=text_process)),
+    ('tfidf', TfidfTransformer()),  # integer counts to weighted TF-IDF scores
+    # train on TF-IDF vectors w/ Naive Bayes classifier
+    ('spam_detect_model', MultinomialNB()),
+])
+
+data = ['I was hoping to work on an B.Tech Project under your guidance in the summers. I would be highly obliged to you if could allot a time slot to meet with me to discuss the same.']
+vector = vectorMatrix.transform(data)
+mess = tfidf.transform(vector).toarray()
+print(spam_detect_model.predict(mess))
+
+# print(pipeline.predict(['I was hoping to work on an B.Tech Project under your guidance in the summers. I would be highly obliged to you if could allot a time slot to meet with me to discuss the same.']))
 
