63e012a520f4a260a91d401ff3312b97b6036915
Gagandeep Singh <gagandeep16037@iiitd.ac.in>
2019-09-22 01:41:02 +0530

    Model modifications

--- spam-or-ham-email-classification.py
+++ spam-or-ham-email-classification.py
@@ -1,17 +1,12 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
 import os
 print(os.listdir("../input"))
 
-dataset = pd.read_csv(r'../input/emails.csv')
+dataset = pd.read_csv('./emails.csv')
 dataset .columns #Index(['text', 'spam'], dtype='object')
 dataset.shape  #(5728, 2)
 
